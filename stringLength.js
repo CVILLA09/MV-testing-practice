14f59d720d23f640e84374482f57a7fc15e2960b
Cristian Villa <c.villavirgen@gmail.com>
2023-07-25 22:23:17 -0600

    Add input validation to stringLength function

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,8 @@
 function stringLength(string) {
+    if (string.length < 1 || string.length > 10) {
+        throw new Error('String length must be between 1 and 10 characters');
+    }
     return string.length;
 }
 
-module.exports = stringLength; // this line is important for importing the function in the test file
+module.exports = stringLength;
