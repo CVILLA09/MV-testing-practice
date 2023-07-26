3cfad74cc4b164869a2558faacfe9374546ab11a
Cristian Villa <c.villavirgen@gmail.com>
2023-07-25 22:25:32 -0600

    Add tests for input validation in stringLength function

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,13 @@
-const stringLength = require('./stringLength'); // this line imports the function from stringLength.js
+const stringLength = require('./stringLength');
 
 test('returns the length of a string', () => {
     expect(stringLength('hello')).toBe(5);
+});
+
+test('throws error when string length is less than 1', () => {
+    expect(() => stringLength('')).toThrow('String length must be between 1 and 10 characters');
+});
+
+test('throws error when string length is more than 10', () => {
+    expect(() => stringLength('morethantencharacters')).toThrow('String length must be between 1 and 10 characters');
 });
